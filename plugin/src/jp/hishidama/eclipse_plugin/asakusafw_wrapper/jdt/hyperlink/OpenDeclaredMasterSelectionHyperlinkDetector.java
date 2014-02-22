19e32c20c69f740376629fa44e979d221dce3b55
hishidama <hishi.dama@asahi.email.ne.jp>
2014-02-22 14:56:40 +0900

    CompilationUnitHyperlinkDetectorに変更

--- plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/jdt/hyperlink/OpenDeclaredMasterSelectionHyperlinkDetector.java
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/jdt/hyperlink/OpenDeclaredMasterSelectionHyperlinkDetector.java
@@ -1,41 +1,18 @@
 package jp.hishidama.eclipse_plugin.asakusafw_wrapper.jdt.hyperlink;
 
+import jp.hishidama.eclipse_plugin.jdt.hyperlink.CompilationUnitHyperlinkDetector;
+
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.javaeditor.WorkingCopyManager;
 import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.texteditor.ITextEditor;
 
-@SuppressWarnings("restriction")
-public class OpenDeclaredMasterSelectionHyperlinkDetector extends AbstractHyperlinkDetector {
+public class OpenDeclaredMasterSelectionHyperlinkDetector extends CompilationUnitHyperlinkDetector {
 
-	public IHyperlink[] detectHyperlinks(ITextViewer textViewer, IRegion region, boolean canShowMultipleHyperlinks) {
-		ITextEditor editor = (ITextEditor) getAdapter(ITextEditor.class);
-		if (editor == null) {
-			return null;
-		}
-		ICompilationUnit unit = getCompilationUnit(editor);
-		if (unit == null) {
-			return null;
-		}
-
-		return detectHyperlinks(unit, region);
-	}
-
-	protected ICompilationUnit getCompilationUnit(ITextEditor editor) {
-		IEditorInput input = editor.getEditorInput();
-		WorkingCopyManager manager = JavaPlugin.getDefault().getWorkingCopyManager();
-		return manager.getWorkingCopy(input, false);
-	}
-
-	private IHyperlink[] detectHyperlinks(ICompilationUnit unit, IRegion region) {
+	@Override
+	protected IHyperlink[] detectHyperlinks(ICompilationUnit unit, IRegion region) {
 		MasterSelectionFinder finder = new MasterSelectionFinder(unit, region.getOffset());
 		String selectionName = finder.getMasterSelectionName();
 		if (selectionName != null) {
