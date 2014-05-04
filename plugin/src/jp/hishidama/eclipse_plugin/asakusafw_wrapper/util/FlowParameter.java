6dac3b9ad3698cac0a20eeccab4661eb05397a9c
hishidama <hishi.dama@asahi.email.ne.jp>
2014-05-04 21:58:37 +0900

    Added FlowUtil

---
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/FlowParameter.java
@@ -0,0 +1,71 @@
+package jp.hishidama.eclipse_plugin.asakusafw_wrapper.util;
+
+public class FlowParameter {
+
+	private String name;
+	private String type;
+	private String modelClassName;
+
+	private String porterName;
+	private String porterClassName;
+
+	public boolean isPort() {
+		return isIn() || isOut();
+	}
+
+	public boolean isIn() {
+		return type.startsWith(FlowUtil.IN_NAME);
+	}
+
+	public boolean isOut() {
+		return type.startsWith(FlowUtil.OUT_NAME);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModelClassName() {
+		if (modelClassName == null) {
+			int n = type.indexOf('<');
+			if (n < 0) {
+				modelClassName = "";
+			} else {
+				int m = type.lastIndexOf('>');
+				if (m < 0) {
+					m = type.length();
+				}
+				modelClassName = type.substring(n + 1, m);
+			}
+		}
+		return modelClassName;
+	}
+
+	public String getPorterName() {
+		return porterName;
+	}
+
+	public void setPorterName(String porterName) {
+		this.porterName = porterName;
+	}
+
+	public String getPorterClassName() {
+		return porterClassName;
+	}
+
+	public void setPorterClassName(String porterClassName) {
+		this.porterClassName = porterClassName;
+	}
+}
---
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/FlowUtil.java
@@ -0,0 +1,78 @@
+package jp.hishidama.eclipse_plugin.asakusafw_wrapper.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import jp.hishidama.eclipse_plugin.jdt.util.AnnotationUtil;
+import jp.hishidama.eclipse_plugin.jdt.util.TypeUtil;
+
+import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.ILocalVariable;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+
+/**
+ * JobFlow/FlowPartユーティリティー.
+ * 
+ * @since 2014.05.04
+ */
+public class FlowUtil {
+	public static final String JOBFLOW_NAME = "com.asakusafw.vocabulary.flow.JobFlow";
+	public static final String FLOW_DESCRIPTION_NAME = "com.asakusafw.vocabulary.flow.FlowDescription";
+	public static final String IMPORT_NAME = "com.asakusafw.vocabulary.flow.Import";
+	public static final String EXPORT_NAME = "com.asakusafw.vocabulary.flow.Export";
+	public static final String IN_NAME = "com.asakusafw.vocabulary.flow.In";
+	public static final String OUT_NAME = "com.asakusafw.vocabulary.flow.Out";
+
+	public static boolean isJobFlow(IType type) {
+		return AnnotationUtil.getAnnotation(type, JOBFLOW_NAME) != null
+				&& TypeUtil.isExtends(type, FLOW_DESCRIPTION_NAME);
+	}
+
+	public static List<FlowParameter> getFlowParameters(IType type) {
+		IMethod constructor = getConstructor(type);
+		if (constructor == null) {
+			return Collections.emptyList();
+		}
+
+		try {
+			ILocalVariable[] parameterList = constructor.getParameters();
+			List<FlowParameter> list = new ArrayList<FlowParameter>(parameterList.length);
+			for (ILocalVariable parameter : parameterList) {
+				String name = parameter.getElementName();
+				String typeName = TypeUtil.getVariableTypeName(parameter);
+				IAnnotation porter = AnnotationUtil.getAnnotation(type, parameter, IMPORT_NAME);
+				if (porter == null) {
+					porter = AnnotationUtil.getAnnotation(type, parameter, EXPORT_NAME);
+				}
+
+				FlowParameter element = new FlowParameter();
+				element.setName(name);
+				element.setType(typeName);
+				element.setPorterName(AnnotationUtil.<String> getAnnotationValue(porter, "name"));
+				element.setPorterClassName(TypeUtil.resolveTypeName(
+						AnnotationUtil.<String> getAnnotationValue(porter, "description"), type));
+
+				list.add(element);
+			}
+			return list;
+		} catch (JavaModelException e) {
+			return Collections.emptyList();
+		}
+	}
+
+	private static IMethod getConstructor(IType type) {
+		try {
+			for (IMethod method : type.getMethods()) {
+				if (method.isConstructor()) {
+					return method;
+				}
+			}
+		} catch (JavaModelException e) {
+			return null;
+		}
+		return null;
+	}
+}
