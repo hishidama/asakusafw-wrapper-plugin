283cc487382bf9649784ba54c404bd24f742dcd3
hishidama <hishi.dama@asahi.email.ne.jp>
2014-05-17 11:43:09 +0900

    Added BatchUtil

---
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/BatchUtil.java
@@ -0,0 +1,12 @@
+package jp.hishidama.eclipse_plugin.asakusafw_wrapper.util;
+
+/**
+ * Batchユーティリティー.
+ * 
+ * @since 2014.05.17
+ */
+public class BatchUtil {
+	public static final String BATCH_NAME = "com.asakusafw.vocabulary.batch.Batch";
+	public static final String BATCH_DESCRIPTION_NAME = "com.asakusafw.vocabulary.batch.BatchDescription";
+	public static final String WORK_NAME = "com.asakusafw.vocabulary.batch.Work";
+}
