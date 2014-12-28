35c8dd627e18545d95ac3947d05a70a7f07ab1bf
hishidama <hishi.dama@asahi.email.ne.jp>
2014-12-28 18:00:58 +0900

    Added BatchUtil.isBatch()

--- plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/BatchUtil.java
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/BatchUtil.java
@@ -1,5 +1,10 @@
 package jp.hishidama.eclipse_plugin.asakusafw_wrapper.util;
 
+import jp.hishidama.eclipse_plugin.jdt.util.AnnotationUtil;
+import jp.hishidama.eclipse_plugin.jdt.util.TypeUtil;
+
+import org.eclipse.jdt.core.IType;
+
 /**
  * Batchユーティリティー.
  * 
@@ -9,4 +14,9 @@ public class BatchUtil {
 	public static final String BATCH_NAME = "com.asakusafw.vocabulary.batch.Batch";
 	public static final String BATCH_DESCRIPTION_NAME = "com.asakusafw.vocabulary.batch.BatchDescription";
 	public static final String WORK_NAME = "com.asakusafw.vocabulary.batch.Work";
+
+	public static boolean isBatch(IType type) {
+		return AnnotationUtil.getAnnotation(type, BATCH_NAME) != null
+				&& TypeUtil.isExtends(type, BATCH_DESCRIPTION_NAME);
+	}
 }
