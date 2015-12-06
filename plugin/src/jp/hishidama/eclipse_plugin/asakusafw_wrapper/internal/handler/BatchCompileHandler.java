a5cc61d52950aaff439aa55581f94501fa15a2c7
hishidama <hishi.dama@asahi.email.ne.jp>
2015-12-06 11:56:39 +0900

    BatchCompileHandler: Added sparkCompileBatchapps

--- plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/internal/handler/BatchCompileHandler.java
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/internal/handler/BatchCompileHandler.java
@@ -1,5 +1,6 @@
 package jp.hishidama.eclipse_plugin.asakusafw_wrapper.internal.handler;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -28,6 +29,9 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -38,7 +42,7 @@ import org.eclipse.ui.services.IServiceLocator;
 
 public class BatchCompileHandler extends AbstractHandler {
 
-	private List<IType> beforeTypeList = Collections.emptyList();
+	private BatchList beforeTypeList = null;
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -46,15 +50,30 @@ public class BatchCompileHandler extends AbstractHandler {
 			return null;
 		}
 
-		List<IType> typeList = getBatchType(event);
+		BatchList result = getBatchType(event);
+		List<IType> typeList = result.typeList;
+		List<IJavaElement> elements = result.elementList;
 		if (typeList.isEmpty()) {
-			typeList = beforeTypeList;
+			if (beforeTypeList != null) {
+				result = beforeTypeList;
+				typeList = beforeTypeList.typeList;
+				elements = beforeTypeList.elementList;
+			}
 		}
 		if (typeList.isEmpty()) {
 			MessageDialog.openInformation(null, "Batch compile", "クラスが見つかりませんでした。\nバッチクラスを選択してから実行して下さい。");
 			return null;
 		}
-		beforeTypeList = typeList;
+		beforeTypeList = result;
+
+		String action = event.getParameter("jp.hishidama.asakusafwWrapper.command.batchCompile.action");
+		if ("compileBatchapp".equalsIgnoreCase(action)) {
+			launchCompileBatchapp(elements);
+			return null;
+		} else if ("sparkCompileBatchapp".equalsIgnoreCase(action)) {
+			launchSparkCompileBatchapp(elements);
+			return null;
+		}
 
 		IProject project = typeList.get(0).getJavaProject().getProject();
 		launchCompileJava(project);
@@ -63,14 +82,23 @@ public class BatchCompileHandler extends AbstractHandler {
 		return null;
 	}
 
-	private List<IType> getBatchType(ExecutionEvent event) {
+	private static class BatchList {
+		public List<IType> typeList;
+		public List<IJavaElement> elementList;
+	}
+
+	private BatchList getBatchType(ExecutionEvent event) {
 		List<IJavaElement> elements = JdtUtil.getJavaElements(event);
 		List<IType> typeList = new ArrayList<IType>(elements.size());
 		for (IJavaElement element : elements) {
 			List<IType> list = getBatchType(element);
 			typeList.addAll(list);
 		}
-		return typeList;
+
+		BatchList result = new BatchList();
+		result.typeList = typeList;
+		result.elementList = elements;
+		return result;
 	}
 
 	private List<IType> getBatchType(IJavaElement element) {
@@ -138,20 +166,29 @@ public class BatchCompileHandler extends AbstractHandler {
 			return;
 		}
 
-		IServiceLocator serviceLocator = PlatformUI.getWorkbench();
+		launchAsakusafwTask("compileJava");
+	}
 
+	private void launchAsakusafwTask(String taskName, String... args) {
+		StringBuilder sb = new StringBuilder(taskName);
+		for (String arg : args) {
+			sb.append(' ');
+			sb.append(arg);
+		}
+
+		IServiceLocator serviceLocator = PlatformUI.getWorkbench();
 		try {
 			ICommandService commandService = (ICommandService) serviceLocator.getService(ICommandService.class);
 			Command command = commandService.getCommand("com.asakusafw.shafu.ui.buildProject");
-			Parameterization[] params = { new Parameterization(command.getParameter("taskNames"), "compileJava") };
+			Parameterization[] params = { new Parameterization(command.getParameter("taskNames"), sb.toString()) };
 			ParameterizedCommand parametrizedCommand = new ParameterizedCommand(command, params);
 
 			IHandlerService handlerService = (IHandlerService) serviceLocator.getService(IHandlerService.class);
 			handlerService.executeCommand(parametrizedCommand, null);
 		} catch (Exception e) {
 			LogUtil.logWarn("Shafu execute error", e);
-			MessageDialog.openWarning(null, "Batch compile : compileJava",
-					"ShafuのcompileJavaコマンドの実行に失敗しました。\nShafuがインストールされているかどうか確認して下さい。");
+			MessageDialog.openWarning(null, "Batch compile : " + taskName,
+					MessageFormat.format("Shafuの{0}コマンドの実行に失敗しました。\nShafuがインストールされているかどうか確認して下さい。", taskName));
 		}
 	}
 
@@ -168,5 +205,62 @@ public class BatchCompileHandler extends AbstractHandler {
 			}
 		};
 		job.schedule();
+	}
+
+	void launchCompileBatchapp(List<IJavaElement> elements) {
+		CompileBatchLauncher launcher = new CompileBatchLauncher("compileBatchapp");
+		launcher.launch(elements);
+	}
+
+	void launchSparkCompileBatchapp(List<IJavaElement> elements) {
+		CompileBatchLauncher launcher = new CompileBatchLauncher("sparkCompileBatchapps");
+		launcher.launch(elements);
+	}
+
+	private class CompileBatchLauncher {
+		private final String taskName;
+
+		public CompileBatchLauncher(String taskName) {
+			this.taskName = taskName;
+		}
+
+		public void launch(List<IJavaElement> elements) {
+			for (IJavaElement element : elements) {
+				if (element instanceof IJavaProject || element instanceof IPackageFragmentRoot) {
+					launchAsakusafwTask(taskName);
+					return;
+				}
+			}
+			for (IJavaElement element : elements) {
+				String name = getBatchClassName(element);
+				if (name != null) {
+					launchAsakusafwTask(taskName, "--update", name);
+				}
+			}
+		}
+
+		private String getBatchClassName(IJavaElement element) {
+			if (element instanceof IType) {
+				IType type = (IType) element;
+				if (BatchUtil.isBatch(type)) {
+					return type.getFullyQualifiedName();
+				}
+			} else if (element instanceof ICompilationUnit) {
+				ICompilationUnit unit = (ICompilationUnit) element;
+				try {
+					for (IType type : unit.getTypes()) {
+						if (BatchUtil.isBatch(type)) {
+							return type.getFullyQualifiedName();
+						}
+					}
+				} catch (JavaModelException e) {
+					// fall through
+				}
+			} else if (element instanceof IPackageFragment) {
+				IPackageFragment pack = (IPackageFragment) element;
+				return pack.getElementName() + ".*";
+			}
+			return null;
+		}
 	}
 }
