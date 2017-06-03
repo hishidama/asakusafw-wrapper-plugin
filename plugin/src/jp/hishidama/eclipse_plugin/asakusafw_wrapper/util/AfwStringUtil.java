dc2c5aff5755987e2f96719c4d19ed839e5f64da
hishidama <hishi.dama@asahi.email.ne.jp>
2017-06-03 10:32:32 +0900

    AfwStringUtil: Added extractTypeParameter()

--- plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/AfwStringUtil.java
+++ plugin/src/jp/hishidama/eclipse_plugin/asakusafw_wrapper/util/AfwStringUtil.java
@@ -7,6 +7,18 @@ public class AfwStringUtil {
 			return null;
 		}
 
+		String generics = extractTypeParameter(text);
+		if (generics != null) {
+			return generics;
+		}
+		return text;
+	}
+
+	public static String extractTypeParameter(String text) {
+		if (text == null) {
+			return null;
+		}
+
 		int s = text.indexOf('<');
 		if (s >= 0) {
 			int e = text.indexOf('>', s + 1);
@@ -15,6 +27,6 @@ public class AfwStringUtil {
 			}
 			return text.substring(s + 1, e);
 		}
-		return text;
+		return null;
 	}
 }
